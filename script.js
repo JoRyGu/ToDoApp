4326fd8cde7909bf233213e2052d440ea1f7d34a
Josh Gude <gude.joshua@gmail.com>
2018-09-07 02:26:02 -0400

    cleaned up JS

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-addButton = document.querySelector('#add-task');
-taskList = document.querySelector('ul');
+function createTaskElement() {
+    let li = document.createElement('li');
+    li.setAttribute('class', 'task-box');
+    let taskInput = document.createElement('input');
+    taskInput.setAttribute('type', 'text');
+    taskInput.setAttribute('id', 'add-task');
+    taskInput.setAttribute('placeholder', 'Add a task...');
+    li.appendChild(taskInput);
+    let editButton = document.createElement('button');
+    editButton.innerText = 'I';
+    editButton.setAttribute('id', 'edit');
+    li.appendChild(editButton);
+    let deleteButton = document.createElement('button');
+    deleteButton.innerText = 'X';
+    deleteButton.setAttribute('id', 'delete');
+    li.appendChild(deleteButton);
+    return li;
+}
 
-addButton.addEventListener('click', () => {
-    let newTask = document.createElement('li');
-    taskList.insertBefore(newTask, addButton);
-    newTask.setAttribute('class', 'task-box');
-    
+const addTaskButton = document.getElementById('start');
+const taskListContainer = document.getElementById('list');
+let taskBox = document.querySelectorAll('.task-box');
+
+addTaskButton.addEventListener('click', () => {
+    let listElement = createTaskElement();
+    taskListContainer.appendChild(listElement);
+    addTaskButton.innerText = 'Add another task';
+    taskBox = document.querySelectorAll('.task-box');
 });
+
